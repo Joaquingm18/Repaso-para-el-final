674a73d9c1df3100b0d3a918449422b91c5b2f2f
Joaquingm18 <joa18gm@gmail.com>
2019-12-06 16:09:36 -0500

    Avance

--- E1.py
+++ E1.py
@@ -1,2 +1,14 @@
 import os, random
 os.system("cls")
+
+n=-1
+
+while n<=0:
+    n=int(input("Ingrese un numero mayor a cero. "))
+    if n<=0:
+        print("Error , ingrese un numero que cumpla lo pedido. ")
+print("")        
+print("Su número es: "+ str(n))
+
+k= random.randint(2,9)
+print("Valor aletorio generado es: "+ str(k))
